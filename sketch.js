97d8c127d2d16c0f66316a2125d27df576ad66d8
StrungOwl <sydneymaeparks@gmail.com>
2024-10-04 19:31:51 -0500

    Sydney Made Changes
    Adding in a method so that you can drop a certain amount of bodies in each country.

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@
   Russia 5 incidents, 158 deaths, gun control but strong black market
   total incidents 425, total deaths 584
   */
- 
+
 let uk_show = true;
 let canada_show = false;
 let germany_show = false;
@@ -15,20 +15,23 @@ let france_show = false;
 let russia_show = false;
 let us_show = false;
 
-let click_count = 0;
-let percent_Us_incidents = 410/425 * 100;
-let percent_UK_incidents = 1/425 * 100; 
-let percent_Canada_incidents = 2/425 * 100;
-let percent_Germany_incidents = 3/425 * 100;
-let percent_France_incidents = 4/425 * 100;
-let percent_Russia_incidents = 5/425 * 100;
+//Animation booleans
+let russiaAnimDone = false;
 
-let percent_Us_deaths = 410/584 * 100;
-let percent_UK_deaths = 0/584 * 100;  
-let percent_Canada_deaths = 2/584 * 100;
-let percent_Germany_deaths = 7/584 * 100;
-let percent_France_deaths = 7/584 * 100;
-let percent_Russia_deaths = 158/584 * 100;
+let click_count = 0;
+let percent_Us_incidents = (410 / 425) * 100;
+let percent_UK_incidents = (1 / 425) * 100;
+let percent_Canada_incidents = (2 / 425) * 100;
+let percent_Germany_incidents = (3 / 425) * 100;
+let percent_France_incidents = (4 / 425) * 100;
+let percent_Russia_incidents = (5 / 425) * 100;
+
+let percent_Us_deaths = (410 / 584) * 100;
+let percent_UK_deaths = (0 / 584) * 100;
+let percent_Canada_deaths = (2 / 584) * 100;
+let percent_Germany_deaths = (7 / 584) * 100;
+let percent_France_deaths = (7 / 584) * 100;
+let percent_Russia_deaths = (158 / 584) * 100;
 
 let uk_width = 20;
 let uk_height = 20;
@@ -48,60 +51,107 @@ let country_array = [];
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
-  uk = new Country(0, width/2, height/2, uk_width, uk_height, '#E72B2B', 0, 0);
+  uk = new Country(
+    0,
+    width / 2,
+    height / 2,
+    uk_width,
+    uk_height,
+    "#E72B2B",
+    0,
+    0
+  );
   country_array.push(uk);
-  canada = new Country(2, width/2, height/2, canada_width, canada_height, '#CD2121', uk.width, 0);
+  canada = new Country(
+    2,
+    width / 2,
+    height / 2,
+    canada_width,
+    canada_height,
+    "#CD2121",
+    uk.width,
+    0
+  );
   country_array.push(canada);
-  germany = new Country( 7, width/2, height/2, germany_width, germany_height,'#B41818', uk.width + canada.width, 0);
+  germany = new Country(
+    7,
+    width / 2,
+    height / 2,
+    germany_width,
+    germany_height,
+    "#B41818",
+    uk.width + canada.width,
+    0
+  );
   country_array.push(germany);
-  france = new Country(7, width/2, height/2, france_width, france_height,'#B41818', uk.width + canada.width + germany.width, 0);
+  france = new Country(
+    7,
+    width / 2,
+    height / 2,
+    france_width,
+    france_height,
+    "#B41818",
+    uk.width + canada.width + germany.width,
+    0
+  );
   country_array.push(france);
-  russia = new Country(158, width/2, height/2, russia_width, russia_height, '#9A0E0E', uk.width + canada.width + germany.width + france.width, 0);
+  russia = new Country(
+    158,
+    width / 2,
+    height / 2,
+    russia_width,
+    russia_height,
+    "#9A0E0E",
+    uk.width + canada.width + germany.width + france.width,
+    0
+  );
   country_array.push(russia);
-  us = new Country(410, width/2, height/2, us_width, us_height, '#800404', uk.width + canada.width + germany.width + france.width + russia.width, 0);
+  us = new Country(
+    410,
+    width / 2,
+    height / 2,
+    us_width,
+    us_height,
+    "#800404",
+    uk.width + canada.width + germany.width + france.width + russia.width,
+    0
+  );
   country_array.push(us);
-  time = millis(); + 100
+  time = millis();
+  +100;
 }
 
 function draw() {
- background(0);
+  background(0);
 
- if (uk_show){
-   uk.display();
-   uk.animate_country_moving();
-   
+  if (uk_show) {
+    uk.display();
+    uk.animate_country_moving();
   }
-  if (canada_show){
+  if (canada_show) {
     canada.display();
     canada.animate_country_moving();
-    
   }
-  if (germany_show){
+  if (germany_show) {
     germany.display();
     germany.animate_country_moving();
-    
   }
-  if (france_show){
+  if (france_show) {
     france.display();
     france.animate_country_moving();
-    
   }
-  if (russia_show){
+  if (russia_show) {
     russia.display();
     russia.animate_country_moving();
-    
+    if(russia.animDone){
+    russia.dropBodies(10);
+    }
   }
-  if (us_show){
+  if (us_show) {
     us.display();
     us.animate_country_moving();
-    
   }
-
 }
-
-
-
-
 
 function mousePressed() {
   click_count++;
@@ -118,27 +168,34 @@ function mousePressed() {
   } else if (click_count == 6) {
     us_show = true;
   }
-  if(uk_show){
-    uk.animate_country_moving();
-  }
-  if(canada_show){
-    canada.animate_country_moving();
-  }
-  if(germany_show){
-    germany.animate_country_moving();
-  }
-  if(france_show){
-    france.animate_country_moving();
-  }
-  if(russia_show){
-    russia.animate_country_moving();
-  }
+  // if (uk_show) {
+  //   uk.animate_country_moving();
+  // }
+  // if (canada_show) {
+  //   canada.animate_country_moving();
+  // }
+  // if (germany_show) {
+  //   germany.animate_country_moving();
+  // }
+  // if (france_show) {
+  //   france.animate_country_moving();
+  // }
+  // if (russia_show) {
+  //  russia.animate_country_moving();
+  // }
 }
 
-
-
 class Country {
-  constructor(num_deaths, x_pos, y_pos, width, height, color, target_x, target_y) {
+  constructor(
+    num_deaths,
+    x_pos,
+    y_pos,
+    width,
+    height,
+    color,
+    target_x,
+    target_y
+  ) {
     this.num_deaths = num_deaths;
     this.x_pos = x_pos;
     this.y_pos = y_pos;
@@ -150,11 +207,58 @@ class Country {
     this.current_square_index = 0;
     this.interval = 500; // Time interval in milliseconds
     this.last_time = 0;
+
+    //For bodies to drop
+    this.animDone = false;
+    this.bodySize = height*0.1;
+
+    this.minSpeed = height*0.001;
+    this.maxSpeed = height*0.01;
+    this.bodySpeed = random(this.minSpeed, this.maxSpeed);
+
+
+    // Initialize an array to store the positions of the ellipses
+    this.bodyPositions = [];
+    this.initializeBodyPositions();
+  }
+
+  initializeBodyPositions() {
+    let minX = this.target_x + this.bodySize;
+    let maxX = this.target_x + this.width - this.bodySize;
+
+    for (let i = 0; i <= this.num_deaths; i++) {
+
+    
+      let bodyX = random(minX, maxX); 
+      let bodyY = 0;
+      let bodySpeed = random(height*0.001, height*0.01);
+      this.bodyPositions.push({ x: bodyX, y: bodyY, speed: bodySpeed });
+    }
   }
 
   display() {
     fill(this.color);
     rect(this.x_pos, this.y_pos, this.width, this.height);
+  }
+
+  dropBodies(numBodies) {
+    for (let i = 0; i < numBodies && i < this.bodyPositions.length; i++) {
+      fill(255);
+      ellipse(
+        this.bodyPositions[i].x,
+        this.bodyPositions[i].y,
+        this.bodySize,
+        this.bodySize
+      );
+
+      //make bodies fall
+      this.bodyPositions[i].y += this.bodyPositions[i].speed;
+
+      if(this.bodyPositions[i].y > this.height - this.bodySize/2){
+        this.bodyPositions[i].speed = 0;
+      }
+
+    }
   }
 
   // animate_death_count() {
@@ -174,6 +278,7 @@ class Country {
   // }
 
   animate_country_moving() {
+
     let speed = 2;
     if (this.x_pos != this.target_x || this.y_pos != this.target_y) {
       let dx = this.target_x - this.x_pos;
@@ -185,6 +290,10 @@ class Country {
       if (abs(dx) < speed) this.x_pos = this.target_x;
       if (abs(dy) < speed) this.y_pos = this.target_y;
     }
+    if(this.x_pos == this.target_x && this.y_pos == this.target_y){
+      this.animDone = true; 
+    }
+
   }
 }
 
